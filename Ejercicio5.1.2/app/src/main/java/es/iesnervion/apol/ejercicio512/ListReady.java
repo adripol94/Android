3d7c980fb461a3ed1e180eed805d33632aeb6b24
adripol94 <apol@iesnervion.es>
2016-11-09 00:32:29 +0100

    JSON Update

---
+++ Ejercicio5.1.2/app/src/main/java/es/iesnervion/apol/ejercicio512/ListReady.java
@@ -0,0 +1,11 @@
+package es.iesnervion.apol.ejercicio512;
+
+/**
+ * Created by adripol94 on 11/8/16.
+ */
+
+public interface ListReady {
+    public String getName();
+    public String getCity();
+    public int getImg();
+}
---
+++ Ejercicio5.1.2/app/src/main/java/es/iesnervion/apol/ejercicio512/ViewHolder.java
@@ -0,0 +1,33 @@
+package es.iesnervion.apol.ejercicio512;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by adripol94 on 11/8/16.
+ */
+
+public class ViewHolder {
+    private ImageView imgTeam;
+    private TextView title;
+    private TextView desc;
+
+    public ViewHolder(View row, int imgId, int titleId, int descId) {
+        imgTeam = (ImageView) row.findViewById(imgId);
+        title = (TextView) row.findViewById(titleId);
+        desc = (TextView) row.findViewById(descId);
+    }
+
+    public ImageView getImgTeam() {
+        return imgTeam;
+    }
+
+    public TextView getTitle() {
+        return title;
+    }
+
+    public TextView getDesc() {
+        return desc;
+    }
+}
