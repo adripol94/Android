8d386cf8f49b89020ea1306cb62be216d078f1d4
apol <adripol1994@gmail.com>
2016-11-24 14:38:31 +0100

    Final de ListTeamsFragment, por terminar MainActivity

--- Ejercicio5.2/app/src/main/java/es/iesnervion/ejercicio52/MainActivity.java
+++ Ejercicio5.2/app/src/main/java/es/iesnervion/ejercicio52/MainActivity.java
@@ -5,15 +5,16 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
 
 
+//ATC: FragmentActivity extendida!!!!!!!!!!!!!!!!!
 
-public class MainActivity extends ListActivity {
-    private Team[] teams;
+public class MainActivity extends FragmentActivity {
 
 
     @Override
@@ -24,9 +25,6 @@ public class MainActivity extends ListActivity {
         toolbar.setTitle(R.string.app_name);
         // Cambiar el color de la letra del toolbar por aquí, de otra forma no se llega a cambiar!
         toolbar.setTitleTextColor(getResources().getColor(R.color.colorAccent));
-
-        // Carga el JSON
-        teams = new LoadJSON(this, "nba.json").getListArray("Teams");
 
         // Comprobación del tipo de pantalla en la que está, para ello llamamos a el fragment movil
         // (el .xml que deberia de salir en caso de que sea un movil y si no esta nulo significa
@@ -43,37 +41,28 @@ public class MainActivity extends ListActivity {
 
             // Creamos una instancia del fragment lista y lo añadimos en la actividad destinada
             // para el movil.
-            //TODO Crear instancia y instanciarlo
 
             try {
-                ListTeamsFragment list = new ListTeamsFragment().newInstance(teams);
+                ListTeamsFragment list = new ListTeamsFragment();
+
+                // Hereda setArguments desde Fragment
+                list.setArguments(getIntent().getExtras());
+
+                // Con FragmentManager podremos interactuar entre el fragment_movil y la clase list
+                // gracias a esto pondremos todos las propiedades preparada de ese
+                // fragment en el fragment
+                getSupportFragmentManager().beginTransaction().add(R.id.fragment_movil, list)
+                        .commit();
+
             } catch (ClassCastException e) {
                 Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
             }
-
         }
-
     }
 
+    public void onTeamSelected(Team team) {
+        //TODO Terminar!!! : Depende de DescriptionFragment
 
-    /**
-     * This method will be called when an item in the list is selected.
-     * Subclasses should override. Subclasses can call
-     * getListView().getItemAtPosition(position) if they need to access the
-     * data associated with the selected item.
-     *
-     * @param l        The ListView where the click happened
-     * @param v        The view that was clicked within the ListView
-     * @param position The position of the view in the list
-     * @param id       The row id of the item that was clicked
-     */
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
 
-        Team selectedTeam = teams[position];
-        Intent intent = new Intent(this, TeamDescription.class);
-        intent.putExtra("class", selectedTeam);
-        startActivity(intent);
     }
 }
