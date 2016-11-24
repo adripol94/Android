12fb7d923ad757c5f2dd8c4adf79839e580432d6
apol <adripol1994@gmail.com>
2016-11-24 14:39:19 +0100

    Final de ListTeamsFragment, por terminar MainActivity

--- Ejercicio5.2/app/src/main/java/es/iesnervion/ejercicio52/DescriptionFragment.java
+++ Ejercicio5.2/app/src/main/java/es/iesnervion/ejercicio52/DescriptionFragment.java
@@ -1,10 +1,122 @@
 package es.iesnervion.ejercicio52;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.text.util.Linkify;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
+ * TODO Terminar!
  * Created by adripol94 on 11/23/16.
  */
 
 public class DescriptionFragment extends Fragment {
+    final static String ARG_TEAM = "team";
+    private Team mTeamActual = null;
+
+    /**
+     * Called to have the fragment instantiate its user interface view.
+     * This is optional, and non-graphical fragments can return null (which
+     * is the default implementation).  This will be called between
+     * {@link #onCreate(Bundle)} and {@link #onActivityCreated(Bundle)}.
+     * <p>
+     * <p>If you return a View from here, you will later be called in
+     * {@link #onDestroyView} when the view is being released.
+     *
+     * @param inflater           The LayoutInflater object that can be used to inflate
+     *                           any views in the fragment,
+     * @param container          If non-null, this is the parent view that the fragment's
+     *                           UI should be attached to.  The fragment should not add the view itself,
+     *                           but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     *                           from a previous saved state as given here.
+     * @return Return the View for the fragment's UI, or null.
+     */
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Comprobamos que no haya instancia, que no venga de un onStop, de ser así lo recogemos
+        if (savedInstanceState != null) {
+            mTeamActual = savedInstanceState.getParcelable(ARG_TEAM);
+        }
+
+        // Inflamos la vista y la devolvemos
+        return inflater.inflate(R.layout.content_team_description, container, false);
+    }
+
+    /**
+     * Called when the Fragment is visible to the user.  This is generally
+     * tied to {@link Activity#onStart() Activity.onStart} of the containing
+     * Activity's lifecycle.
+     */
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // Comprobamos que no venga ningun argumento y lo recogemos
+        Bundle args = getArguments();
+
+        if (args != null) {
+            updateDescriptionView(args.getParcelable(ARG_TEAM));
+        } else if (mTeamActual != null) {
+            updateDescriptionView(mTeamActual);
+        }
+    }
+
+    /**
+     * Called to ask the fragment to save its current dynamic state, so it
+     * can later be reconstructed in a new instance of its process is
+     * restarted.  If a new instance of the fragment later needs to be
+     * created, the data you place in the Bundle here will be available
+     * in the Bundle given to {@link #onCreate(Bundle)},
+     * {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}, and
+     * {@link #onActivityCreated(Bundle)}.
+     * <p>
+     * <p>This corresponds to {@link Activity#onSaveInstanceState(Bundle)
+     * Activity.onSaveInstanceState(Bundle)} and most of the discussion there
+     * applies here as well.  Note however: <em>this method may be called
+     * at any time before {@link #onDestroy()}</em>.  There are many situations
+     * where a fragment may be mostly torn down (such as when placed on the
+     * back stack with no UI showing), but its state will not be saved until
+     * its owning activity actually needs to save its state.
+     *
+     * @param outState Bundle in which to place your saved state.
+     */
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        // Para guardar la instancia guardamos el objeto en si
+        outState.putParcelable(ARG_TEAM, mTeamActual);
+    }
+
+    private void updateDescriptionView(Parcelable team) {
+        Team t = (Team) team;
+
+        ImageView img = (ImageView) getActivity().findViewById(R.id.imgCabecera);
+        TextView title = (TextView) getActivity().findViewById(R.id.titleTeam);
+        TextView desc = (TextView) getActivity().findViewById(R.id.descriptionTeam);
+        TextView link = (TextView) getActivity().findViewById(R.id.linkTeam);
+
+        Toolbar toolbar = (Toolbar) getActivity().findViewById(R.id.toolbar_description);
+
+        img.setImageResource(t.getImg());
+        title.setText(t.getName());
+        desc.setText(t.getCity());
+
+
+        toolbar.setTitle(t.getName().toString()); //Añade al toolbar un nombre especifico(Nombre del equipo)
+        link.setText(t.getWeb());
+        desc.setText(t.getCity());
+
+        Linkify.addLinks(link, Linkify.WEB_URLS); //Pone formato de nombre a url
+    }
 }
--- Ejercicio5.2/app/src/main/java/es/iesnervion/ejercicio52/ListTeamsFragment.java
+++ Ejercicio5.2/app/src/main/java/es/iesnervion/ejercicio52/ListTeamsFragment.java
@@ -13,7 +13,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 
 // Ya que se tratat de una lista tenemos que añadir la libreria de ListFragment
@@ -21,7 +23,7 @@ import android.view.ViewGroup;
 public class ListTeamsFragment extends ListFragment {
     private Team[] t;
 
-    private OnHeadSelectedListener mClaaBack;
+    private OnHeadSelectedListener mCallBack;
 
     /**
      * Interfaz diseñada para implementar que la clase escuche cuando se le clickea a una opc de la lista
@@ -73,13 +75,35 @@ public class ListTeamsFragment extends ListFragment {
         int layout = Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ?
                 android.R.layout.simple_list_item_activated_1 : android.R.layout.simple_list_item_1;
 
-        // Recoger el valor por el bundle que se le introdujo desde newInstance
+        // Recoger el valor por el bundle que se le introdujo desde newInstance o en caso contrario
+        // crear un LoadJSON y leer el json y recoger el Array
         if (getArguments() != null) {
             t = (Team[]) getArguments().getParcelableArray("teams");
+        } else {
+            t = new LoadJSON(getContext(), "nba.json").getListArray("teams");
         }
 
         // Crear la lista
         setListAdapter(new ListAdapter<Team>(getActivity(), R.layout.row, R.id.tittle, t));
+    }
+
+    /**
+     * Called when the Fragment is visible to the user.  This is generally
+     * tied to {@link Activity#onStart() Activity.onStart} of the containing
+     * Activity's lifecycle.
+     */
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // Comprobamos si el segundo fragment es nullo, de no ser así se indicará que estamos en una
+        // tablet
+        // De ser así indicaremos con CHOICE_MODE_SINGLE que se quedará seleccionada en la lista
+        // la opcion seleccionada.
+        if (getFragmentManager().findFragmentById(R.id.fragment_descriptions) != null) {
+            getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        }
+
     }
 
     /**
@@ -91,20 +115,37 @@ public class ListTeamsFragment extends ListFragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+
+        // Castear el context, de no sér así no saltará la excepcion
+        try {
+            mCallBack = (OnHeadSelectedListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException("No implementado OnHeadSelectedListener :("
+                    + e.getMessage() + ")");
+        }
     }
-
-
 
     /**
-     * Called when the Fragment is visible to the user.  This is generally
-     * tied to {@link Activity#onStart() Activity.onStart} of the containing
-     * Activity's lifecycle.
+     * This method will be called when an item in the list is selected.
+     * Subclasses should override. Subclasses can call
+     * getListView().getItemAtPosition(position) if they need to access the
+     * data associated with the selected item.
+     *
+     * @param l        The ListView where the click happened
+     * @param v        The view that was clicked within the ListView
+     * @param position The position of the view in the list
+     * @param id       The row id of the item that was clicked
      */
     @Override
-    public void onStart() {
-        super.onStart();
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        //TODO Terminar!!
+        //Introducimos la posicion en la interfaz
+        mCallBack.onArticleSelected(position);
+
+        //Indicamos al ListView que posicion de la lista tiene que seleccionar.
+        // Indicaremos con true que ponga como seleccionado la posicion pasada por parametro
+        getListView().setItemChecked(position, true);
     }
-
-
-
 }
