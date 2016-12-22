348bc666713d39734413871d51ae45170b8d7307
apol <adripol1994@gmail.com>
2016-12-22 13:42:14 +0100

    OnResumen End

--- Ejercicio5.2/app/src/main/java/es/iesnervion/ejercicio52/View/Activity/MainActivity.java
+++ Ejercicio5.2/app/src/main/java/es/iesnervion/ejercicio52/View/Activity/MainActivity.java
@@ -55,7 +55,6 @@ public class MainActivity extends AppCompatActivity implements ListTeams.OnHeadT
     /** Manejadora de {@link Player} */
     private PlayerManager manager;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -109,6 +108,24 @@ public class MainActivity extends AppCompatActivity implements ListTeams.OnHeadT
         });
 
     }
+
+    /**
+     * Dispatch onResume() to fragments.  Note that for better inter-operation
+     * with older versions of the platform, at the point of this call the
+     * fragments attached to the activity are <em>not</em> resumed.  This means
+     * that in some cases the previous state may still be saved, not allowing
+     * fragment transactions that modify the state.  To correctly interact
+     * with fragments in their proper state, you should instead override
+     * {@link #onResumeFragments()}.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+
+    }
+
 
     /**
      * Dispatch incoming result to the correct fragment.
